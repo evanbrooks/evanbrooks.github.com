b8879a72aaf890dba1247d1b50900b08b99fcd9e
Evan <brooks.ev@gmail.com>
2013-01-07 16:02:43 -0500

    hide address bar on iOS, try 2

--- js.js
+++ js.js
@@ -333,7 +333,7 @@ window.addEventListener("load",function() {
   setTimeout(function(){
     // Hide the address bar!
     window.scrollTo(0, 1);
-  }, 0);
+  }, 1000);
 });
 
 /* =========== Stepwise Plugin =========== */
