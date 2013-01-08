fde194169f6a35e53edbfd56f56b1a0c919a5f91
Evan <brooks.ev@gmail.com>
2013-01-07 20:05:14 -0500

    switch from classes to data-attributes to store state data

--- js.js
+++ js.js
@@ -267,35 +267,43 @@ $(function(){
 
 	function toggleBox(e){
 		e.preventDefault();
+		var $wrapper = $(".box-contents");
+		var boxState = $wrapper.attr("data-box");
+		var spreadState = $wrapper.attr("data-spread");
 		var $box = $(".box-fold");
-		if ( $box.hasClass("unfolded") ){
+		if ( spreadState == "spread") {
+			toggleSpread();
+		}
+		else if ( boxState == "unfolded" ){
 			$(".box-fold").parent().removeClass("box-open");
 			goHere = $(".box-top-point").position().top;
     		$viewScroll.animate({scrollTop: goHere}, 'slow');
 			$(".box-fold > .frames").stepWise('backward', 6, 600, function(){
 			});
 			$box.removeClass("unfolded");
+			$wrapper.attr("data-box", "folded");
 		}
 		else {
 			$(".box-fold > .frames").stepWise('forward', 6, 600, function(){
 				$(".box-fold").parent().addClass("box-open");
 			});
 			$box.addClass("unfolded");
+			$wrapper.attr("data-box", "unfolded");
 		}
 	}
 
 	function toggleSpread(e){
-		var $wrapper = $(".box-items");
-		var currState = $wrapper.attr("data-spread");
+		var $wrapper = $(".box-contents");
+		var spreadState = $wrapper.attr("data-spread");
 		var goHere;
-		if (currState == "open") {
-			$wrapper.attr("data-spread", "closed").parent().removeClass("box-spread");
+		if (spreadState == "spread") {
+			$wrapper.attr("data-spread", "stacked").removeClass("box-spread");
 			goHere = $(".box-top-point").position().top;
 			// 							  ^ don't use offset() in a scrollable div
 		}
 		else {
-			$wrapper.attr("data-spread", "open").parent().addClass("box-spread");
-			goHere = $(".box-top-point").position().top + $wrapper.prev().height();
+			$wrapper.attr("data-spread", "spread").addClass("box-spread");
+			goHere = $(".box-fold").position().top + $(".box-fold").height();
 			// 					 ^ don't use offset() in a scrollable div
     	}
     	$viewScroll.animate({scrollTop: goHere}, 'slow');
